7e89706f8d22c5703a1da3076ad11b93b717f9f4
Kamil Sabbagh <kamil.mousa9@gmail.com>
2023-11-02 16:33:31 +0300

    update

--- distill.py
+++ distill.py
@@ -492,7 +492,7 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    for num_of_images in [1, 10, 20, 30, 40, 50]:
+    for num_of_images in [10, 20, 30, 40, 50]:
         for _ in range(10):
             args.ipc = num_of_images
             main(args, num_of_images)
