c946a95d37c32fd2797eae18906bde8d46410b05
Kamil Sabbagh <kamil.mousa9@gmail.com>
2023-11-09 16:56:24 +0300

    update

--- print_eval_distill.py
+++ print_eval_distill.py
@@ -1,6 +1,6 @@
 import pandas as pd
+import numpy as np
 import os
-#import matplotlib.pyplot as plt
 
 # List of paths to your CSV files
 file_paths = [
@@ -13,30 +13,40 @@ file_paths = [
     "./ipc30/class_accuracies_ConvNet.csv"
 ]
 
-data = []
+# Define a threshold for detecting outliers
+std_dev_threshold = 2  # for example, any data point more than 2 standard deviations
 
 # Iterate through each file path
 for path in file_paths:
+    # Check if the path exists
+    if not os.path.exists(path):
+        print(f"File not found: {path}")
+        continue
+
     # Read the CSV file into a DataFrame
-    print(path)
-    print(os.path.exists(path))
     df = pd.read_csv(path)
     
-    # Calculate the average of each row and append to averages list
-    avg = df.mean(axis=1).tolist()
-    data.append(avg)
+    # Calculate the mean and standard deviation for each class
+    means = df.mean()
+    std_devs = df.std()
 
-    # Print the average of each row for the current CSV file
-    print(f"{path} average is:")
-    print(avg)
+    # Identify outliers for each class
+    outliers = np.abs(df - means) > std_dev_threshold * std_devs
+    
+    # Filter out the outliers
+    filtered_df = df[~outliers.any(axis=1)]
+    num_outliers_discarded = len(df) - len(filtered_df)
+
+    # Calculate the average mean and variance for each class after filtering
+    filtered_means = filtered_df.mean()
+    filtered_variances = filtered_df.var()
+
+    # Print the number of outliers discarded
+    print(f"{path} - Outliers discarded: {num_outliers_discarded}")
+    
+    # Print the average mean and variance for each class
+    print(f"{path} - Average mean per class after filtering outliers:")
+    print(filtered_means)
+    print(f"{path} - Average variance per class after filtering outliers:")
+    print(filtered_variances)
     print("-----")
-
-# # Plot the averages
-# for idx, avg in enumerate(averages):
-#     plt.plot(avg, label=f'File {idx + 1}')
-
-# plt.legend()
-# plt.title("Average of each row for each file")
-# plt.xlabel("Row Index")
-# plt.ylabel("Average Value")
-# plt.show()
