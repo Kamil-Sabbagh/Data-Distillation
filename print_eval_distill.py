909c80e2c994f45c54082ad69491810ca4f81e74
Kamil Sabbagh <kamil.mousa9@gmail.com>
2023-11-09 17:00:52 +0300

    update

--- print_eval_distill.py
+++ print_eval_distill.py
@@ -2,6 +2,12 @@ import pandas as pd
 import numpy as np
 import os
 
+# Define the class names if they are not present in the CSV file
+class_names = [
+    'airplane', 'automobile', 'bird', 'cat', 'deer', 
+    'dog', 'frog', 'horse', 'ship', 'truck'
+]
+
 # List of paths to your CSV files
 file_paths = [
     "./ipc1/class_accuracies_ConvNet.csv",
@@ -23,16 +29,16 @@ for path in file_paths:
         print(f"File not found: {path}")
         continue
 
-    # Read the CSV file into a DataFrame
-    df = pd.read_csv(path)
+    # Read the CSV file into a DataFrame, assuming no header is present
+    df = pd.read_csv(path, header=None, names=class_names)
     
     # Calculate the mean and standard deviation for each class
     means = df.mean()
     std_devs = df.std()
 
     # Identify outliers for each class
-    outliers = np.abs(df - means) > std_dev_threshold * std_devs
-    
+    outliers = (np.abs(df - means) > std_dev_threshold * std_devs)
+
     # Filter out the outliers
     filtered_df = df[~outliers.any(axis=1)]
     num_outliers_discarded = len(df) - len(filtered_df)
@@ -46,7 +52,7 @@ for path in file_paths:
     
     # Print the average mean and variance for each class
     print(f"{path} - Average mean per class after filtering outliers:")
-    print(filtered_means)
+    print(filtered_means.values)
     print(f"{path} - Average variance per class after filtering outliers:")
     print(filtered_variances)
     print("-----")
